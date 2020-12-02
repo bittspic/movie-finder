6588af903d9db3dff58675dee28cc3e7f6dd051e
Hien Nguyen <bittspic@gmail.com>
2020-12-03 01:32:36 +0700

    movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Movie from "./Component/Movie";
 
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8a4f09eab279107e0bef68fb90dbbcc2&page=1";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=8a4f09eab279107e0bef68fb90dbbcc2&query=";
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  async function fetchData() {
+    let resp = await fetch(FEATURED_API);
+    let data = await resp.json();
+    console.log(data);
+    setMovies(data.results);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (searchTerm) {
+      (async () => {
+        let resp = await fetch(SEARCH_API + searchTerm);
+        let data = await resp.json();
+        setMovies(data.results);
+      })();
+    }
+    setSearchTerm("");
+  }
+
+  function handleChange(e) {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={searchTerm}
+            className="search"
+            type="text"
+            placeholder="Search..."
+          ></input>
+        </form>
       </header>
+      <div className="movie-container">
+        {movies.map((movie) => (
+          <Movie key={movie.id} {...movie} />
+        ))}
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/Component/Movie.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({
+  title,
+  poster_path,
+  overview,
+  vote_average,
+  release_date,
+}) => (
+  <div className="movie">
+    <img src={IMAGE_API + poster_path} alt={title}></img>
+    <div className="movie-info">
+      <h3>{title}</h3>
+      <span>{vote_average}</span>
+    </div>
+    <div className="movie-over">
+      <h2>Overview</h2>
+      <p>{overview}</p>
+    </div>
+  </div>
+);
+
+export default Movie;
